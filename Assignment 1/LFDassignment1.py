2f8ca69b0cc7eb3d24baa63d4957c119784f7003
erikbijl <bijlerik@hotmail.com>
2018-09-05 14:38:20 +0200

    solved subplots

--- Assignment 1/LFDassignment1.py
+++ Assignment 1/LFDassignment1.py
@@ -9,9 +9,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
-
-# This function plots a confusion matrix
-def plot_confusion_matrix (cm, classes,
+# This function plots a confusion matrix 
+def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
@@ -30,6 +29,41 @@ def plot_confusion_matrix (cm, classes,
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# This function plots a confusion matrix without a colorbar to not have a colorbar at each subplot
+def plot_confusion_matrix_without_colorbar (cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    #plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
@@ -169,10 +203,10 @@ for train_index, test_index in kf.split(X):
   cnf_matrix = confusion_matrix(y_test, y_guess)
   np.set_printoptions(precision=2)
 
-  plt.subplot(320+fold)
+  plt.subplot(3,2,fold)
   # Plot non-normalized confusion matrix
-  plt.imshow(float(plot_confusion_matrix(cnf_matrix, classes=set(Ytest),
-                      title='Confusion matrix leaving out fold '+str(fold))), cmap=plt.cm.BuPu_r)
+  plot_confusion_matrix_without_colorbar(cnf_matrix, classes=set(Ytest),
+                      title='fold'+str(fold))
   fold = fold + 1
 
 plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
